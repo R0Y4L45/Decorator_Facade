6c9c4c9200b91bec40f18518a3b0397b9bcb06ec
R0Y4L45 <106491312+R0Y4L45@users.noreply.github.com>
2022-12-02 14:29:04 +0400

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,119 @@
+﻿public interface MobileShop
+{
+    public void modelNo();
+    public void price();
+}
+
+public class Iphone : MobileShop
+{
+    public void modelNo()
+    {
+        Console.WriteLine(" Iphone 6 ");
+    }
+    public void price()
+    {
+        Console.WriteLine(" Rs 65000.00 ");
+    }
+}
+
+
+public class Samsung : MobileShop
+{
+    public void modelNo()
+    {
+        Console.WriteLine(" Samsung galaxy tab 3 ");
+    }
+    public void price()
+    {
+        Console.WriteLine(" Rs 45000.00 ");
+    }
+}
+
+public class Blackberry : MobileShop
+{
+    public void modelNo()
+    {
+        Console.WriteLine(" Blackberry Z10 ");
+    }
+    public void price()
+    {
+        Console.WriteLine(" Rs 55000.00 ");
+    }
+}
+
+
+public class ShopKeeper
+{
+    private MobileShop iphone;
+    private MobileShop samsung;
+    private MobileShop blackberry;
+
+    public ShopKeeper()
+    {
+        iphone= new Iphone();
+        samsung=new Samsung();
+        blackberry=new Blackberry();
+    }
+    public void iphoneSale()
+    {
+        iphone.modelNo();
+        iphone.price();
+    }
+    public void samsungSale()
+    {
+        samsung.modelNo();
+        samsung.price();
+    }
+    public void blackberrySale()
+    {
+        blackberry.modelNo();
+        blackberry.price();
+    }
+}
+
+
+public class FacadePatternClient
+{
+    private static int choice;
+    public static void Main()
+    {
+        do
+        {
+            Console.WriteLine("========= Mobile Shop ============ \n");
+            Console.WriteLine("            1. IPHONE.              \n");
+            Console.WriteLine("            2. SAMSUNG.              \n");
+            Console.WriteLine("            3. BLACKBERRY.            \n");
+            Console.WriteLine("            4. Exit.                     \n");
+            Console.WriteLine("Enter your choice: ");
+
+            choice = int.Parse(Console.ReadLine()!);
+            
+            ShopKeeper sk = new ShopKeeper();
+
+            switch (choice)
+            {
+                case 1:
+                    {
+                        sk.iphoneSale();
+                    }
+                    break;
+                case 2:
+                    {
+                        sk.samsungSale();
+                    }
+                    break;
+                case 3:
+                    {
+                        sk.blackberrySale();
+                    }
+                    break;
+                default:
+                    {
+                        Console.WriteLine("Nothing You purchased");
+                    }
+                    return;
+            }
+
+        } while (choice!=4);
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace Decarator;
+public interface Vehicle
+{
+    string Make { get; }
+    string Model { get; }
+    double Price { get; }
+}
+
+/// ConcreteComponent
+public class X5 : Vehicle
+{
+    public string Make
+    {
+        get { return "X5"; }
+    }
+
+    public string Model
+    {
+        get { return "CNG"; }
+    }
+
+    public double Price
+    {
+        get { return 1000000; }
+    }
+}
+
+/// Decorator
+public abstract class VehicleDecorator : Vehicle
+{
+    private Vehicle _vehicle;
+
+    public VehicleDecorator(Vehicle vehicle)
+    {
+        _vehicle = vehicle;
+    }
+
+    public string Make
+    {
+        get { return _vehicle.Make; }
+    }
+
+    public string Model
+    {
+        get { return _vehicle.Model; }
+    }
+
+    public double Price
+    {
+        get { return _vehicle.Price; }
+    }
+
+}
+
+
+//The ConcreteDecorator
+
+public class SpecialOffer : VehicleDecorator
+{
+    public int DiscountPercentage { get; set; }
+    public string? Offer { get; set; }
+
+    public double Price
+    {
+        get
+        {
+            double price = base.Price;
+            int percentage = 100 - DiscountPercentage;
+            return Math.Round((price * percentage) / 100, 2);
+        }
+    }
+
+    public SpecialOffer(Vehicle vehicle) : base(vehicle) { }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        X5 car = new X5();
+
+        Console.WriteLine($"Price : {car.Price}");
+
+        SpecialOffer offer = new SpecialOffer(car);
+        offer.DiscountPercentage = 25;
+        offer.Offer = "25 % discount";
+
+        Console.WriteLine($"{offer.Offer}  Special Offer and price are : {offer.Price} ");
+
+    }
+}
